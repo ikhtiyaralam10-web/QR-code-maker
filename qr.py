560bbff450bedd14cfcb4690ac8f4ce43a55ad3e
ikhtiyaralam10-web <ikhtiyaralam10@gmail.com>
2025-11-23 19:17:38 +0530

    added the python file

---
+++ qr.py
@@ -0,0 +1,11 @@
+import qrcode
+
+link = input("Enter the link: ").strip()
+file_path ="C:\\Users\\ikhti\\OneDrive\\Desktop\\qr code\\qrcode.png"
+
+qr = qrcode.QRCode()
+qr.add_data(link)
+
+img = qr.make_image()
+img.save(file_path)
+
